5c96c34e26c8dd249a7d8f82ec02d488a540f868
zhangzhaofu <zhang_zhao_fu@sina.com>
2020-05-21 15:41:09 +0800

    Add testnet addr.

--- src/Bitcoin/Entry.cpp
+++ src/Bitcoin/Entry.cpp
@@ -59,6 +59,7 @@ string Entry::normalizeAddress(TWCoinType coin, const string& address) const {
 string Entry::deriveAddress(TWCoinType coin, const PublicKey& publicKey, TW::byte p2pkh, const char* hrp) const {
     switch (coin) {
         case TWCoinTypeBitcoin:
+        case TWCoinTypeBitcoinTestnet:
         case TWCoinTypeDigiByte:
         case TWCoinTypeLitecoin:
         case TWCoinTypeViacoin:
